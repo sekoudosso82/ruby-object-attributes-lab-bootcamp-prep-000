44d60d324236171b586faf799e3538e4efff376e
Sekou Dosso <sekou.dosso82@gmail.com>
2019-12-29 16:09:05 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog 
+  
+  def initialize(name,breed)
+    @name = name 
+    @breed = breed 
+  end 
+  
+  def name 
+    @name
+  end 
+  
+  def breed
+    @breed
+  end 
+  
+  def name=(value)
+    @name=value
+  end 
+  
+  def breed=(value)
+    @breed=value
+  end
+  
+end 
